522ef890c72fcb0ec0d1948c63c414d5d3274bf6
Elmira <elmiraramiz2018@gmail.com>
2026-04-14 13:59:23 +0300

    update api

--- api.py
+++ api.py
@@ -3,19 +3,23 @@ from database import add_transaction, get_all_transactions, delete_transaction
 
 app = FastAPI()
 
+
 @app.get("/")
 def home():
     return {"message": "Finance Tracker API"}
+
 
 @app.get("/transactions")
 def list_all():
     rows = get_all_transactions()
     return {"transactions": rows}
 
+
 @app.post("/add")
-def add(amount: float, category: str, type: str, date: str, description: str = ""):
-    add_transaction(amount, category, type, date, description)
+def add(amount: float, category: str, typee: str, date: str, description: str = ""):
+    add_transaction(amount, category, typee, date, description)
     return {"status": "success"}
+
 
 @app.delete("/delete/{tid}")
 def delete(tid: int):
